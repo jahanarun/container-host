5ed7a1a0c3c6ad48a683c94276eac45a2b721bd8
jahanarun <jahanarun@dexome.com>
2021-06-06 20:53:55 +0530

    Makes docker to wait for plex executable.

--- plex/cmd.ps1
+++ plex/cmd.ps1
@@ -2,4 +2,6 @@ reg import c:\plex\plex.reg
 Write-Output Sleeping...
 Start-Sleep -Seconds 60
 ipconfig /all
-& "c:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe"
+$myprocss = Start-Process "c:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe" -PassThru
+$myprocss.WaitForExit()
+
