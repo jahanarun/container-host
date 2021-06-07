bce4835f0b068a910a610fce26fc0c28f15b8027
jahanarun <jahanarun@dexome.com>
2021-06-07 11:20:44 +0530

    Takes registry backup at regular interval.

--- plex/cmd.ps1
+++ plex/cmd.ps1
@@ -1,7 +1,11 @@
-reg import c:\plex\plex.reg
-Write-Output Sleeping...
+if (Test-Path c:\plex\plex.reg) {
+    reg import c:\plex\plex.reg
+}
 Start-Sleep -Seconds 60
 ipconfig /all
-$myprocss = Start-Process "c:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe" -PassThru
-$myprocss.WaitForExit()
+$myprocess = Start-Process "c:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe" -PassThru
+while (!$myprocess.HasExited) {
+    Start-Sleep -Seconds 300
+    reg export "HKCU\Software\Plex, Inc." c:\plex\plex.reg /y
+}
 
