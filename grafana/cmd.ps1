7fb49a7377a1d9acb2de4f07e975e1563b3ad0a0
jahanarun <jahanarun@dexome.com>
2021-06-10 11:19:10 +0530

    Install grafana plugins during container startup.

--- grafana/cmd.ps1
+++ grafana/cmd.ps1
@@ -2,5 +2,9 @@ Start-Sleep -Seconds 60
 Set-Location C:\ProgramData\chocolatey\lib\grafana\tools\
 $grafana_directory = Get-ChildItem -Include grafana* -Directory -Name
 Set-Location ($grafana_directory + "\bin")
+grafana-cli --pluginsDir "c:\config\plugins" plugins install grafana-worldmap-panel
+grafana-cli --pluginsDir "c:\config\plugins" plugins install grafana-piechart-panel
+
+
 $myprocess = Start-Process grafana-server.exe -ArgumentList '--config "C:\config\custom.ini"' -PassThru
 $myprocess.WaitForExit()
