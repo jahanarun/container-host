c4276648f3874193a3a66a5dc3c5d9693386975b
jahanarun <jahanarun@dexome.com>
2021-06-30 11:40:48 +0530

    Revert "Updates args of caddy script."
    This reverts commit 410a0882d09ce45a22f4f459dae8c090660fc59e.

--- caddy/cmd.ps1
+++ caddy/cmd.ps1
@@ -1,4 +1,4 @@
 Start-Sleep -Seconds 60
 ipconfig /all
-$myprocess = Start-Process "c:\caddy\caddy.exe" -ArgumentList 'run', '--config c:\config\caddy\Caddyfile', '--watch' -PassThru
+$myprocess = Start-Process "c:\caddy\caddy.exe" -ArgumentList 'run', '--config c:\config\caddy\Caddyfile' -PassThru
 $myprocess.WaitForExit()
