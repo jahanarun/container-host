62f86610f75ce9a1378ac44b23a60d54a8311ca2
Jahan Arun, J <jahanarun@dexome.com>
2022-07-30 05:09:51 +0530

    Set timeout for plex

--- plex/cmd.ps1
+++ plex/cmd.ps1
@@ -3,6 +3,7 @@ if (Test-Path c:\plex\plex.reg) {
 }
 ipconfig /all
 $myprocess = Start-Process "c:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe" -PassThru
+Start-Sleep -Seconds 3
 while (!$myprocess.HasExited) {
     Start-Sleep -Seconds 300
     reg export "HKCU\Software\Plex, Inc." c:\plex\plex.reg /y
