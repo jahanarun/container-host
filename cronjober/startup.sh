2d00d9e1dba93036953ec0cbc1fe5cef38cf9179
Jahan Arun, J <jahanarunj@dexome.com>
2024-11-10 23:00:19 +0530

    Fix paths for cronjober

--- cronjober/startup.sh
+++ cronjober/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 /scripts/onvif_camera_set_time.py | tee -a /var/log/cron.log 2>&1
-python3 /scripts/tailscale-maintenance.py | tee -a /var/log/cron.log 2>&1
+python3 /cronjober/onvif_camera_set_time.py | tee -a /var/log/cron.log 2>&1
+python3 /cronjober/tailscale-maintenance.py | tee -a /var/log/cron.log 2>&1
