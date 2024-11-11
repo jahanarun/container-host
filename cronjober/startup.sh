5be4f2b6d136559a64cb38f9170dd538109493f3
Jahan Arun, J <jahanarunj@dexome.com>
2024-11-11 10:06:30 +0530

    cronjober: update script

--- cronjober/startup.sh
+++ cronjober/startup.sh
@@ -3,4 +3,4 @@
 python3 /cronjober/onvif_camera_set_time.py | tee -a /var/log/cron.log 2>&1
 python3 /cronjober/tailscale-maintenance.py | tee -a /var/log/cron.log 2>&1
 
-/usr/bin/supercronic /cronjober/crontab
+/usr/bin/supercronic -split-logs -passthrough-logs /cronjober/crontab
