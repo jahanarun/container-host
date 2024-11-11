29655b177f5d04f1f62cbd7eeff7d56d590c33a8
Jahan Arun, J <jahanarunj@dexome.com>
2024-11-11 10:05:48 +0530

    cronjober: Replace crond with supercronic

--- cronjober/startup.sh
+++ cronjober/startup.sh
@@ -2,3 +2,5 @@
 
 python3 /cronjober/onvif_camera_set_time.py | tee -a /var/log/cron.log 2>&1
 python3 /cronjober/tailscale-maintenance.py | tee -a /var/log/cron.log 2>&1
+
+/usr/bin/supercronic /cronjober/crontab
