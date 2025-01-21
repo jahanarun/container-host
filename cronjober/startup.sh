76f13b729d491c6a7e692445c53f73347c803371
Jahan Arun, J <5706717+jahanarun@users.noreply.github.com>
2025-01-21 06:29:05 +0530

    Remove tailscale from startup script

--- cronjober/startup.sh
+++ cronjober/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python3 /cronjober/onvif_camera_set_time.py | tee -a /var/log/cron.log 2>&1
-python3 /cronjober/tailscale-maintenance.py | tee -a /var/log/cron.log 2>&1
+# python3 /cronjober/tailscale-maintenance.py | tee -a /var/log/cron.log 2>&1
 
 /usr/bin/supercronic -split-logs -passthrough-logs /cronjober/crontab
